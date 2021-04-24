18364a41a8ed62d1c57cd9ae18bca0c2de02ea3c
Dario Gonzalez <jdariogonzalez.2020@gmail.com>
2021-04-24 10:35:10 -0300

    agregada validaca que verifica se todos os campos estan preenchidos, add reset dos campos depois do envio das informaçoes

--- src/app/components/crear-cita/crear-cita.component.ts
+++ src/app/components/crear-cita/crear-cita.component.ts
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-cita.component.css']
 })
 export class CrearCitaComponent implements OnInit {
+  nome = '';
+  data = '';
+  hora = '';
+  sintomas = '';
+
+  formularioIncorrecto = false;
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+    //Validação que verifica se todos os campos estão preenchidos
+  agregarCita() {
+    if(this.nome == '' || this.data == '' || this.hora == '' || this.sintomas == ''){
+      this.formularioIncorrecto = true;
+      return;
+    }
+    this.formularioIncorrecto = false;
+    
+    //Criamos um objeto para enviar ao pai
+    const CITA = {
+      nome: this.nome,
+      data: this.data,
+      hora: this.hora,
+      sintomas: this.sintomas
+    }
+    console.log(CITA);
+    this.resetCampos()
+  }
+
+  //Reset os campos após o envio das informações
+  resetCampos() {
+      this.nome = '';
+      this.data = '';
+      this.hora = '';
+      this.sintomas = '';
   }
 
 }
