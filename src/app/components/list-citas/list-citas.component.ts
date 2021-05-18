906c3db132824281037d0d205974d33cdacbac16
Jeanmarie Quijada <jeanmarie.q@gmail.com>
2021-05-17 21:15:24 -0300

    boton apagar uma cita, sem atualizar navegador

--- src/app/components/list-citas/list-citas.component.ts
+++ src/app/components/list-citas/list-citas.component.ts
@@ -15,6 +15,10 @@ export class ListCitasComponent implements OnInit {
   constructor(private crudService: CrudService) { }
 
   ngOnInit(): void {
+   this.getCita();
+  }
+
+  getCita(){
     this.crudService.getCitas().subscribe(res => {
       console.log(res)
       this.citas = res.Citas;
@@ -22,11 +26,12 @@ export class ListCitasComponent implements OnInit {
       );
   }
 
-  deleteCita(id) {
+  deleteCita(id: string) {
     //console.log(id)
     this.crudService.deleteCita(id).subscribe(res => {
-      console.log(res)
-    }, err => console.error(err)
+      console.log(res);
+      this.getCita();
+    }, err => console.log(err)
     );
   }
 
