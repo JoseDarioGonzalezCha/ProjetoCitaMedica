3e36aedd3693b178eb973753c81a7ed1b11a6448
Jeanmarie Quijada <jeanmarie.q@gmail.com>
2021-05-20 00:28:42 -0300

    component edit sem funcionar

--- src/app/components/dados-cita/dados-cita.component.ts
+++ src/app/components/dados-cita/dados-cita.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CrudService } from './../../service/crud.service'
-import { from } from 'rxjs';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Cita } from 'src/app/service/cita';
 import { FormGroup, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-dados-cita',
@@ -12,67 +11,58 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class DadosCitaComponent implements OnInit {
 
-/*   obtenerId: any;
-  atualizarForm: FormGroup; */
+  obtenerId: any;
+  atualizarForm: FormGroup; 
 
-  cita: Cita = {
-    Name: '',
-    date: '',
-    time: '',
-    description: '',
- }; 
- 
   constructor( 
-    public formBuilder: FormBuilder,
+    public formBuilder : FormBuilder,
     private router: Router, 
     private cruidService: CrudService, 
-    private activatedRoute: ActivatedRoute) { 
+    private activatedRoute: ActivatedRoute,
+    ) { 
 
-    
-    
-      /*  this.obtenerId = this.activatedRoute.snapshot.paramMap.get('id');
+    this.obtenerId = this.activatedRoute.snapshot.paramMap.get('id');
+    console.log(this.obtenerId)
 
-      this.cruidService.getCita(this.obtenerId).subscribe(res => {
+    this.cruidService.getCita(this.obtenerId).subscribe(res => {
+      console.log(res)
       this.atualizarForm.setValue({
-        name: res['name'],
+        Name: res['Name'],
         date: res['date'],
         time: res['time'],
         description: res['description']
       });
 
-      }); 
+    }); 
 
-      this.atualizarForm = this.formBuilder.group({
-        name: [''],
+    this.atualizarForm = this.formBuilder.group({
+        Name: [''],
         date: [''],
         time: [''],
         description: ['']
-      }) */
-    }
-
+    })
+    console.log(this.atualizarForm.value)
+  }
   
-  ngOnInit(): void {
-  /*  const params = this.activatedRoute.snapshot.params;
-    if(params.id) {
+  ngOnInit(): void { }
+
+  onUpdateCita(): any {
+    this.cruidService.updateCita(this.obtenerId, this.atualizarForm.value)
+    .subscribe(() => {
+        console.log('data update'); 
+        this.router.navigateByUrl('/list-citas');
+      }, err => {
+        console.log(err);
+      });
+  } 
+  //mostra no console a cita mais no completa em formulario
+ /*  if(params.id) {
       this.cruidService.getCita(params.id)
       .subscribe(
         res => {
           console.log(res);
           this.cita = res;
-        }, err => console.error(err)
-      )
-    }   */
-  }
-
-  /* /* onUpdateCita() {
-    this.cruidService.updateCita(this.obtenerId, this.atualizarForm.value)
-    .subscribe( () => {
-        console.log(res); 
-        this.router.navigate(['/list-citas']);
-      }, err => console.error(err)
-    )
-   
-  }  */
- 
+        }, err => console.error(err) 
+      ) }  */ 
 
 }
--- src/app/service/crud.service.ts
+++ src/app/service/crud.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
 import { Cita } from './cita'
+
 
 @Injectable({
   providedIn: 'root'
