c5dab4c7788af16de31d0ca65819910cd0d5ee41
Jeanmarie Quijada <jeanmarie.q@gmail.com>
2021-05-22 01:48:50 -0300

    reestruturação de código create-cita

--- src/app/components/crear-cita/crear-cita.component.ts
+++ src/app/components/crear-cita/crear-cita.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { CrudService } from './../../service/crud.service';
 import {FormGroup } from '@angular/forms';
 import { Cita } from 'src/app/service/cita';
@@ -21,49 +21,24 @@ export class CrearCitaComponent implements OnInit {
   formularioIncorrecto = false;
 
 
-  // @Output() nuevaCita = new EventEmitter<any>(); // decorador @Output para enviar "CITA" ao pai, por meio da classe EventEmiter, passando um objeto do tipo eny.
+  
  
- 
-  constructor( private router: Router, private cruidService: CrudService) { }
+  constructor( private router: Router, private cruidService: CrudService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
-   
+  
   }
     
   createNewCita() {
-     this.cruidService.createCita(this.cita)
-      .subscribe( res => {
-        console.log(res);
-        this.router.navigate(['/list-citas']);
-      }, err => console.error(err)
-      )
-    }
-   /*  //Validação que verifica se todos os campos estão preenchidos
-    agregarCita() {
-    if(this.nome == '' || this.data == '' || this.hora == '' || this.sintomas == ''){
-      this.formularioIncorrecto = true;
-      return;
-    }
-    this.formularioIncorrecto = false;
-    
-    //Criamos um objeto para enviar ao pai
-    const CITA = {
-      nome: this.nome,
-      data: this.data,
-      hora: this.hora,
-      sintomas: this.sintomas
-    }
-    console.log(CITA);
-    this.nuevaCita.emit(CITA);
-    this.resetCampos()
+  this.cruidService.createCita(this.cita)
+    .subscribe( res => {
+      console.log(res);
+      this.router.navigate(['/list-citas']);
+    }, err => console.error(err)
+    )
   }
 
-  //Reset os campos após o envio das informações
-  resetCampos() {
-      this.Name = '';
-      this.date = '';
-      this.time = '';
-      this. description = '';
-  }  */
+
+
 
 }
